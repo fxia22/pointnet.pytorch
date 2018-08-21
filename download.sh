155c3dd36befaadedc90f4c530a8ceeec01d88b6
Jingpeng Wu <jingpeng.wu@gmail.com>
2018-08-21 18:34:02 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
-wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip
+wget https://shapenet.cs.stanford.edu/ericyi/shapenetcore_partanno_segmentation_benchmark_v0.zip --no-check-certificate
 unzip shapenetcore_partanno_segmentation_benchmark_v0.zip
 rm shapenetcore_partanno_segmentation_benchmark_v0.zip
